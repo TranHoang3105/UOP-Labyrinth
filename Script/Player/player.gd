8492a2d6893e7e0e345e6d86296efca4e565dcb7
ZORO-209 <a_sanchezbello@u.pacific.edu>
2025-11-21 20:04:41 -0800

    3rd person animations, walk,running(shift), and idle

--- Script/Player/player.gd
+++ Script/Player/player.gd
@@ -1,19 +1,41 @@
 extends CharacterBody3D
 
+@onready var camera_mount = $camera_mount
+@onready var animation_player: AnimationPlayer = $CollisionShape3D/Visual/mixamo_base/AnimationPlayer
+@onready var visual: Node3D = $CollisionShape3D/Visual
 
-const SPEED = 5.0
+
+
+var SPEED = 3.0
 const JUMP_VELOCITY = 4.5
 var sens_horizontal = 0.5
+
+var walking_speed = 3.0
+var running_speed = 5.0
+
+var running = false;
+
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	
 func _input(event):
 	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(-event.relative.x * sens_horizontal))
+		rotate_y(deg_to_rad(event.relative.x * sens_horizontal))
+		visual.rotate_y(deg_to_rad(-event.relative.x * sens_horizontal))
 		#$camera_mount.rotate_x(-deg_to_rad(event.relative.y * sens_horizontal))
 		
 		
 func _physics_process(delta: float) -> void:
+		
+	if Input.is_action_just_pressed("sprint"):
+		SPEED = running_speed
+		running = true
+	else:
+		SPEED = walking_speed	
+		running = false
+	
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -27,9 +49,20 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		if running:
+			if animation_player.current_animation != "running":
+				animation_player.play("running")
+		else:
+			if animation_player.current_animation != "walking":
+				animation_player.play("walking")
+			
+		visual.look_at(position + direction)
+		
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
+		if animation_player.current_animation != "idel":
+			animation_player.play("idel")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
